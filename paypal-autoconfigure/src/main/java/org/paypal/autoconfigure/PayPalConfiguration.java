7982da684bfac7b427c8dc958d407348702c6dba
Javier ledo váquez <jledo@paradigmatecnologico.com>
2015-12-04 14:06:14 +0100

    Modulo final

--- paypal-autoconfigure/src/main/java/org/paypal/autoconfigure/PayPalConfiguration.java
+++ paypal-autoconfigure/src/main/java/org/paypal/autoconfigure/PayPalConfiguration.java
@@ -2,7 +2,6 @@ package org.paypal.autoconfigure;
 
 import java.util.Properties;
 
-import javax.management.RuntimeErrorException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +13,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.paypal.base.ConfigManager;
 import com.paypal.base.Constants;
@@ -34,35 +34,25 @@ public class PayPalConfiguration {
 	@Autowired
 	private PayPalProperties payPalProperties;
 	
-	
-	@ConditionalOnClass(Test.class)
-	@ConditionalOnMissingBean
-	@Bean
-	public APIContext paypalApiContextTest(){
-		Properties properties =new Properties();
-		
-		PayPalResource.initConfig(properties);
-		
-	    String accessToken = getAccessToken();
-
-	    APIContext apiContext = new APIContext(accessToken);
-		return apiContext;
-	}
-	
-	@ConditionalOnMissingClass("junit.framework.Test")
 	@ConditionalOnMissingBean
 	@ConditionalOnProperty(matchIfMissing=true, prefix="paypal")
 	@Bean
-	public APIContext paypalApiContextLocal() {
-		Properties properties =new Properties();
-		configurePropertiesLocal(properties);
-		
-		PayPalResource.initConfig(properties);
-		
-	    String accessToken = getAccessToken();
-
-	    APIContext apiContext = new APIContext(accessToken);
-		return apiContext;
+	public APIContext paypalApiContextTest(){
+		boolean production = false;
+		boolean useProperties = false;
+		Properties properties =	configurePropertiesPro(production, useProperties);
+		return initApi(properties);
+	}
+	
+	@ConditionalOnClass(Test.class)
+	@ConditionalOnMissingBean
+	@ConditionalOnProperty(value={"paypal.clientId", "paypal.clientSecret"})
+	@Bean
+	public APIContext paypalApiContextTestProperties(){
+		boolean production = false;
+		boolean useProperties = true;
+		Properties properties =	configurePropertiesPro(production, useProperties);
+		return initApi(properties);
 	}
 	
 	@ConditionalOnMissingClass("junit.framework.Test")
@@ -70,61 +60,19 @@ public class PayPalConfiguration {
 	@ConditionalOnProperty(value={"paypal.clientId", "paypal.clientSecret"})
 	@Bean
 	public APIContext paypalApiContextPro(){
-		Properties properties =new Properties();
-		configurePropertiesPro(properties);
-		
+		boolean production = true;
+		boolean useProperties = true;
+		Properties properties =	configurePropertiesPro(production, useProperties);
+		return initApi(properties);
+	}
+	
+
+	private APIContext initApi(Properties properties) {
 		PayPalResource.initConfig(properties);
-		
-	    String accessToken = getAccessToken();
-
-	    APIContext apiContext = new APIContext(accessToken);
-		return apiContext;
+		return new APIContext(getAccessToken());
 	}
 	
-	
-	private void configurePropertiesPro(Properties properties) {
-		properties.put("http.ConnectionTimeOut", 5000);
-		properties.put("http.Retry", 1);
-		properties.put("http.ReadTimeOut", 30000);
-		properties.put("http.MaxConnection", 100);
-		
-		properties.put("http.ProxyPort", 8080);
-		properties.put("http.ProxyHost", "127.0.0.1");
-		properties.put("http.UseProxy", false);
-		properties.put("http.ProxyUserName", null);
-		properties.put("http.ProxyPassword", null);
-
-		properties.put("http.GoogleAppEngine", false);
-		
-		properties.put("service.EndPoint", "https://api.paypal.com");
-
-		properties.put("clientId", payPalProperties.getClientId());
-		properties.put("clientSecret", payPalProperties.getClientSecret());
-
-	}
-
-	private void configurePropertiesLocal(Properties properties) {
-		properties.put("http.ConnectionTimeOut", 5000);
-		properties.put("http.Retry", 1);
-		properties.put("http.ReadTimeOut", 30000);
-		properties.put("http.MaxConnection", 100);
-		
-		properties.put("http.ProxyPort", 8080);
-		properties.put("http.ProxyHost", "127.0.0.1");
-		properties.put("http.UseProxy", false);
-		properties.put("http.ProxyUserName", null);
-		properties.put("http.ProxyPassword", null);
-
-		properties.put("http.GoogleAppEngine", false);
-		
-		properties.put("service.EndPoint", "https://api.sandbox.paypal.com");
-
-		properties.put("clientId", "AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS");
-		properties.put("clientSecret", "EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL");
-
-	}
-
-	public String getAccessToken()  {
+	private String getAccessToken()  {
 		String clientID = ConfigManager.getInstance().getConfigurationMap().get(Constants.CLIENT_ID);
 		String clientSecret = ConfigManager.getInstance().getConfigurationMap().get(
 				Constants.CLIENT_SECRET);
@@ -136,4 +84,40 @@ public class PayPalConfiguration {
 			throw new RuntimeException("No se puede configurar el token para el acceso al api de paypal", e);
 		}
 	}
+	private Properties configurePropertiesPro(boolean production, boolean useProperties) {
+		Properties properties = new Properties();
+		properties.put("http.ConnectionTimeOut", 5000);
+		properties.put("http.Retry", 1);
+		properties.put("http.ReadTimeOut", 30000);
+		properties.put("http.MaxConnection", 100);
+		
+		properties.put("http.ProxyPort", 8080);
+		properties.put("http.ProxyHost", "127.0.0.1");
+		properties.put("http.UseProxy", false);
+		properties.put("http.ProxyUserName", null);
+		properties.put("http.ProxyPassword", null);
+
+		properties.put("http.GoogleAppEngine", false);
+		
+		if(production){
+			properties.put("service.EndPoint", "https://api.paypal.com");
+		} else {
+			properties.put("service.EndPoint", "https://api.sandbox.paypal.com");
+		}
+
+		if(useProperties){
+			properties.put("clientId", payPalProperties.getClientId());
+			properties.put("clientSecret", payPalProperties.getClientSecret());
+		} else {
+			properties.put("clientId", "AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS");
+			properties.put("clientSecret", "EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL");
+		}
+
+		return properties;
+
+	}
+
+
+
+
 }
